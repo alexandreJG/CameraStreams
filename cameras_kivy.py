8a4239d509d303a575b6e71b6876fddeb3f38cf9
alexandreJG <s200326@student.dhbw-mannheim.de>
2024-04-25 13:33:26 +0200

    two cameras activating

--- cameras_kivy.py
+++ cameras_kivy.py
@@ -69,22 +69,28 @@ class CameraScreen(Screen):
     def __init__(self, **kwargs):
         super(CameraScreen, self).__init__(**kwargs)
         self.layout = BoxLayout(orientation='vertical')
-        self.camera_layout = GridLayout(cols=2)  # This layout is for the cameras
         self.home_button = Button(text='Home')
         self.home_button.bind(on_press=self.go_home)
         self.layout.add_widget(self.home_button)
         self.start_button = Button(text='Start Camera')
         self.start_button.bind(on_press=self.start_camera)
         self.layout.add_widget(self.start_button)
-        #self.layout.add_widget(self.camera_layout)  # Add the camera layout to the main layout
         self.add_widget(self.layout)
         self.is_capturing = False 
 
     def start_camera(self, instance):
+        self.camera_layout = GridLayout(cols=2)  # This layout is for the cameras
         if not self.is_capturing:  
-            self.capture = cv2.VideoCapture(0)
-            self.my_camera = KivyCamera(capture=self.capture, fps=30)
-            self.layout.add_widget(self.my_camera, index=0)
+            self.capture1 = cv2.VideoCapture(0)
+            self.my_camera1 = KivyCamera(capture=self.capture1, fps=30)
+            self.camera_layout.add_widget(self.my_camera1)  # Add the camera to the grid layout
+        
+            self.capture2 = cv2.VideoCapture(1)  # Create a VideoCapture object for the second camera
+            self.my_camera2 = KivyCamera(capture=self.capture2, fps=30)
+            self.camera_layout.add_widget(self.my_camera2)  # Add the second camera to the grid layout
+        
+            self.layout.add_widget(self.camera_layout)  # Add the grid layout to the main layout
+        
             self.button = Button(text='Pause')
             self.button.bind(on_press=self.on_button_press)
             self.layout.add_widget(self.button)
@@ -95,12 +101,14 @@ class CameraScreen(Screen):
         self.manager.current = 'start'
     
     def on_button_press(self, instance):
-        # Pause or resume the capture depending on the current state
-        if self.my_camera.paused:
-            self.my_camera.paused = False
+    # Pause or resume the capture depending on the current state
+        if self.my_camera1.paused and self.my_camera2.paused:
+            self.my_camera1.paused = False
+            self.my_camera2.paused = False
             self.button.text = 'Pause'
         else:
-            self.my_camera.paused = True
+            self.my_camera1.paused = True
+            self.my_camera2.paused = True
             self.button.text = 'Resume'
 
 
